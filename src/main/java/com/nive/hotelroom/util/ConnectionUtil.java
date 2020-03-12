4e864855b9d97e833f9133339251ec262cc48e22
Nivetha M <nivethamoorthy19@gmail.com>
2020-03-12 11:02:51 +0530

    update

--- src/main/java/com/nive/hotelroom/util/ConnectionUtil.java
+++ src/main/java/com/nive/hotelroom/util/ConnectionUtil.java
@@ -2,13 +2,23 @@ package com.nive.hotelroom.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.nive.hotelroom.exception.DBException;
+import com.nive.hotelroom.exception.ErrorConstant;
 
 public class ConnectionUtil {
 
-		public static  Connection getConnect()throws Exception {
+		public static  Connection getConnect() throws ClassNotFoundException, DBException {
 
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-			Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","system","oracle");
+			Connection con;
+			try {
+				con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","system","oracle");
+			} catch (SQLException e) {
+				e.printStackTrace();
+				throw new DBException(ErrorConstant.INVALID_CON);
+			}
 			//Connection con=DriverManager.getConnection("jdbc:oracle:thin:@13.235.147.120:1521:XE","nivetha","nivetha");
 			return con;
 		}
